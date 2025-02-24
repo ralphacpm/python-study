1d2deab5f7b2257bed46e4cee2d9a033c5079f01
Ralph Macalino <macalino47@gmail.com>
2025-02-24 19:07:08 +0800

    learned about input variables and type casting

--- variables.py
+++ variables.py
@@ -5,3 +5,15 @@ father_name = "macalino"
 is_student = True
 age = 25
 print(name + " " + father_name + " " + str(age) + " years old")
+
+# taking inputs for variables
+
+name = input("Enter your name: ")
+print("Hello " + name)  # concatenation
+
+
+# ask user for their age, and tell them their age after 5 years
+
+age = input("Enter your age: ")
+age_after_5_years = int(age) + 5
+print("Your age after 5 years will be " + str(age_after_5_years))
